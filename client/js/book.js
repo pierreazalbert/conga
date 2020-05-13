cdc745b1f206be1bff8a0b213788af9d072c0e92
pierreazalbert <p.azalbert@bboxx.co.uk>
2020-05-13 23:10:48 +0200

    almost done with booking page!

--- client/js/book.js
+++ client/js/book.js
@@ -4,72 +4,103 @@ $(document).ready(async function () {
 	// get shop data
 	var shop = await getShopDataByCode(code);
 	console.log(shop.id, shop.data());
-	// get shop bookings for tomorrow
-	var bookings = await getBookings(shop.id);
+	
+	// listen to shop bookings and update schedule
+	listenBookings(shop);
+
 	// render page
 	renderPage(shop.data());
 	
-	// determine which slots are free
-	var schedule = generateSchedule(shop, bookings);
 	
 });
 
 function renderPage(shop) {
 	$('#shop-name').text(shop.name);
 	$('#shop-address').text(shop.address);
+	
+	const today = new Date();
+	$('#todays-date').text(moment(today).format("dddd Do MMMM YYYY"));
+	var hours = shop.hours[today.toDateString().slice(0,3).toLowerCase()]
+	$('#shop-hours').text('Open ' + hours.open + ' to ' + hours.close);
+}
+
+function renderSchedule(schedule){
+	$('#schedule').empty();
+	schedule.forEach(function (slot) {
+		var count = slot.filter(booking => booking.free == false).length;
+		//console.log(slot[0].time, count, slot.length);
+		if (count == slot.length) {
+			$('#schedule').append('<div class="col-4 col-md-2 p-3 my-2"><button type="button" class="btn btn-secondary disabled btn-lg">' + slot[0].time + '</button></div>');
+		} else if (count < slot.length && count > 0) {
+			$('#schedule').append('<div class="col-4 col-md-2 p-3 my-2"><button type="button" class="btn btn-outline-warning btn-lg">' + slot[0].time + '</button></div>');
+		} else if (count == 0) {
+			$('#schedule').append('<div class="col-4 col-md-2 p-3 my-2"><button type="button" class="btn btn-outline-success btn-lg">' + slot[0].time + '</button></div>');
+		}
+	});
 }
 
 function generateSchedule(shop, bookings) {
 	const today = new Date();
-	var hours = shop.data().hours[today.toDateString().slice(0,3).toLowerCase()]
+	var hours = shop.data().hours[today.toDateString().slice(0,3).toLowerCase()];
 	var params = shop.data().bookings;
-	console.log(params.number, params.rate);
 	
 	// calculate number of slots during the day
 	var slots_n = moment.duration(moment(hours.close, 'HH:mm')-moment(hours.open, 'HH:mm'))/moment.duration(params.rate, 'minutes');
-	console.log("Number of slots during the day: ", slots_n);
 	
 	// generate empty schedule
 	var schedule = new Array(slots_n);
-	console.log(schedule);
-//	schedule.forEach(function (slot, i) {
-//		console.log('i=',i);
-//		console.log('slot=', slot);
-//		for (var j = 0; j < params.number; j++) {
-//			var time = moment(hours.open, 'HH:mm') + i*moment.duration(params.rate, 'minutes') + j*moment.duration(5, 'minutes');
-//			slot.push({'time': moment(time).format('HH:mm'), 'free': true});
-//		}
-//		
-//	});
-	
 	for (var i = 0; i < slots_n; i++) {
 		var slots = new Array();
 		for (var j = 0; j < params.number; j++) {
 			var time = moment(hours.open, 'HH:mm') + i*moment.duration(params.rate, 'minutes') + j*moment.duration(5, 'minutes');
-			slots.push({'time': moment(time).format('HH:mm'), 'free': true});
+			var free = bookings.find(booking => booking.data().time == moment(time).format('HH:mm')) == undefined;
+			slots.push({'time': moment(time).format('HH:mm'), 'free': free});
 		}
 		schedule[i] = slots;
 	}
-	console.log(schedule);
-
+	console.log("Schedule", schedule);
+	return schedule;
 }
 
-async function getBookings(shopID) {
+//async function getBookings(shopID) {
+//	
+//	// we are only interested in getting tomorrow's bookings
+//	const today = new Date();
+//	const tomorrow = new Date(today);
+//	tomorrow.setDate(tomorrow.getDate() + 1);
+//	
+//	// get bookings for specific shop on specific day
+//	var query = await db.collection('tickets').where('shop', '==', shopID).where('date', '==', tomorrow.toLocaleDateString()).get();
+//	if(query.empty) {
+//		console.log("No bookings found for this shop!");
+//		return [];
+//	} else {
+//		console.log("Found " + query.size.toString() + " bookings");
+//		return query.docs;
+//	}
+//}
+
+async function listenBookings(shop) {
+	
+	var schedule = new Array();
 	
 	// we are only interested in getting tomorrow's bookings
 	const today = new Date();
 	const tomorrow = new Date(today);
-	
 	tomorrow.setDate(tomorrow.getDate() + 1);
+	
 	// get bookings for specific shop on specific day
-	var query = await db.collection('tickets').where('shop', '==', shopID).where('date', '==', tomorrow.toLocaleDateString()).get();
-	if(query.empty) {
-		console.log("No bookings found for this shop!");
-		return [];
-	} else {
-		console.log("Found " + query.size.toString() + " bookings");
-		return query.docs;
-	}
+	await db.collection('tickets').where('shop', '==', shop.id).where('date', '==', tomorrow.toLocaleDateString()).onSnapshot(function (bookings) {
+		if(bookings.empty) {
+			console.log("No bookings found for this shop!");
+			schedule = generateSchedule(shop, []);
+		} else {
+			console.log("Found following bookings: ", bookings.docs);
+			schedule = generateSchedule(shop, bookings.docs);
+		}
+		renderSchedule(schedule);
+	});
+
 }
 
 async function getShopDataByCode (code) {
