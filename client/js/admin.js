53dd9539949870a14f284b3eb2abd83181a44458
pierreazalbert <p.azalbert@bboxx.co.uk>
2020-06-11 19:32:53 +0200

    implemented queue status tracking + next customer button + shop favourites

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,5 +1,3 @@
-var today = moment();
-
 $(document).ready(async function () {
 	// get login key
 	var key = getParams(window.location.href).key;
@@ -10,17 +8,59 @@ $(document).ready(async function () {
 	$('#shop-name').text(shop.data().name);
 	$('#shop-code').prepend("Shop ID: " + shop.data().code[0] + "-" + shop.data().code[1] + "-" + shop.data().code[2] + "-" + shop.data().code[3]);
 	$('#shop-link').prop('href', '/book?shop=' + shop.data().code);
-	$('[name=today]').text(today.format("dddd Do MMMM"));
+	$('[name=today]').text(moment().format("dddd Do MMMM"));
 
 	// render next booking & all bookings sections
-	var bookings = await db.collection('tickets').where('shop', '==', shop.id).where('date', '==', today.format("DD/MM/YYYY")).get();
+	var bookings = await db.collection('tickets').where('shop', '==', shop.id).where('date', '==', moment().format("DD/MM/YYYY")).get();
 	if(bookings.empty) {
 		console.log("No bookings found for this shop!");
 		$('#all-bookings').empty().append('<div class="col-12 mb-2 d-flex justify-content-center"><label class="w-100 btn btn-outline-secondary nohover btn-lg">No bookings found for today</label></div>');
 	} else {
 		console.log("Found following bookings: ", bookings.docs);
-		renderBookings(bookings.docs);
+		var sorted = bookings.docs.slice();
+		sorted.sort((a, b) => moment(a.data().time, 'HH:mm') - moment(b.data().time, 'HH:mm'));
+
+		renderQueue(shop.data(), sorted);
+		renderBookings(sorted);
+		makeSchedulePDF(sorted);
+
+		if(shop.data().status == "auto") {
+			$("#next-booking").click();
+			var update = setInterval(function () {
+				renderQueue(shop.data(), sorted);
+				renderBookings(sorted);
+			}, 10000);
+		} else {
+			$("#next-customer").click();
+		}
+
 	}
+
+	$("#toggle-queue").change(async function (event) {
+		if($(':checked').attr('id') == 'next-booking') {
+			console.log('Switching to automatic queue control');
+			shop = await updateShopQueueStatus(shop, 'auto');
+			// switch to auto mode
+
+			renderQueue(shop.data(), sorted);
+			renderBookings(sorted);
+
+			update = setInterval(function () {
+				renderQueue(shop.data(), sorted);
+				renderBookings(sorted);
+			}, 10000);
+
+
+
+		} else if ($(':checked').attr('id') == 'next-customer') {
+			console.log('Switching to manual queue control');
+			shop = await updateShopQueueStatus(shop, 'manual');
+			// if button was inactive, activate manual mode
+			renderQueue(shop.data(), sorted);
+			clearInterval(update);
+
+		}
+	});
 
 	// render shop settings
 	$('#shop-settings > #name').text(shop.data().name);
@@ -38,61 +78,61 @@ $(document).ready(async function () {
 	renderLinks(shop);
 });
 
-function renderLinks(shop) {
-	// copy link button
-	new ClipboardJS('#copy-link');
-	$("#copy-link").attr('data-clipboard-text', 'https://conga.store/book?shop=' + shop.data().code);
-	// qr code and pdf poster
-	var qrcode = new QRCode(document.getElementById("qrcode"), 'https://conga.store/book?shop=' + shop.data().code);
-	$("#qrcode > img ").on('load', function() {
-		var pdf = new jsPDF();
-		pdf.setFontSize(40);
-		pdf.text("Book your visit", 30, 30);
-		pdf.text("with Conga", 30, 50);
-		pdf.addImage(this.src, 30, 100, 100, 100);
-		pdf.text('www.conga.store/' + shop.data().code, 30, 250);
-		$("#get-pdf").on('click', function() {
-			pdf.save('Shop poster ('+ shop.data().code[0] + "-" + shop.data().code[1] + "-" + shop.data().code[2] + "-" + shop.data().code[3] + ')');
-		});
-	});
-}
+async function updateShopQueueStatus(shop, status) {
 
-async function renderBookings(bookings){
-
-	// render next booking
-	var closest = bookings.slice();
-	console.log(closest);
-	closest.sort(function(a, b) {
-	  var dA = Math.abs(moment(a.data().time, 'HH:mm') - today),
-	    dB = Math.abs(moment(b.data().time, 'HH:mm') - today);
-	  if (dA < dB) {
-	    return -1;
-	  } else if (dA > dB) {
-	    return 1;
-	  } else {
-	    return 0;
-	  }
-	});
-
-	if (moment(closest[0].data().time, 'HH:mm') >= moment()) {
-		$('#next-booking').parent().removeClass('d-none');
-		$('#next-booking').find('h1').text('#' + closest[0].data().time.replace(':', ''));
-		$('#next-booking').find('h1').parent().css('background-color', closest[0].data().color);
-		$('#next-booking').find('h1').addClass(lightOrDark(closest[0].data().color) ? 'text-dark':'text-white');
+	try {
+		var doc = await db.collection("shops").doc(shop.id);
+		await doc.update({status: status});
+		console.log("Shop queue status successfully updated!");
+		shop = await doc.get();
+		return shop;
+	} catch (error) {
+		console.error("Error updating shop: ", error);
 	}
 
-	// $('button[name=next]').click(function (sorted) {
-	// 	var current =
-	// });
+
+}
+
+function renderQueue(shop, bookings){
+
+	var closest = bookings.find(booking => moment(booking.data().time, 'HH:mm') >= moment());
+
+	console.log("Queue status", shop.status);
+
+	$('#queue-status').parent().removeClass('d-none');
+
+	if (shop.status == 'auto') {
+
+		if ((closest != undefined) && (moment.duration(moment(closest.data().time, "HH:mm") - moment()).asMinutes() < 2)) {
+
+			console.log('Next booking', closest.data().time);
+			$('#queue-status').find('h1').text('#' + closest.data().time.replace(':', ''));
+			$('#queue-status').find('h1').parent().css('background-color', closest.data().color);
+			$('#queue-status').find('h1').removeClass('text-white text-dark').addClass(lightOrDark(closest.data().color) ? 'text-dark':'text-white');
+
+		} else {
+			$('#queue-status').find('h1').text('PLEASE WAIT').removeClass('text-white text-dark').addClass('text-dark');
+			$('#queue-status').find('h1').parent().css('background-color', '#FF9020');
+		}
+
+	} else {
+		$('#queue-status').find('h1').text('NEXT CUSTOMER').removeClass('text-white text-dark').addClass('text-white');
+		$('#queue-status').find('h1').parent().css('background-color', '#35A000');
+	}
+
+}
+
+function renderBookings(bookings){
 
 	// render all Bookings
-	var sorted = bookings.slice();
-	sorted.sort((a, b) => moment(a.data().time, 'HH:mm') - moment(b.data().time, 'HH:mm'));
-	sorted.forEach(function (ticket) {
+	var closest = bookings.find(booking => moment(booking.data().time, 'HH:mm') >= moment());
+
+	$('#all-bookings').empty();
+	bookings.forEach(function (ticket) {
 		if (moment(ticket.data().time, 'HH:mm') < moment()) {
 			$('#all-bookings').append('<div class="col-4 col-xl-3 p-3 mb-2 d-flex justify-content-center"><label class="w-100 btn btn-outline-secondary nohover btn-lg">#' + ticket.data().time.replace(':', '') + '</label></div>');
 		} else {
-			if (ticket.data().time == closest[0].data().time) {
+			if ((ticket.data().time == closest.data().time) && (moment.duration(moment(closest.data().time, "HH:mm") - moment()).asMinutes() < 2)) {
 				$('#all-bookings').append('<div class="col-4 col-xl-3 p-3 mb-2 d-flex justify-content-center"><label class="w-100 font-weight-bold btn nohover btn-outline-dark btn-lg" style="box-shadow: 0 0 0 5px ' + ticket.data().color + '; border-color: transparent">#' + ticket.data().time.replace(':', '') + '</label></div>');
 			}  else {
 				$('#all-bookings').append('<div class="col-4 col-xl-3 p-3 mb-2 d-flex justify-content-center"><label class="w-100 font-weight-bold btn btn-outline-dark nohover btn-lg">#' + ticket.data().time.replace(':', '') + '</label></div>');
@@ -100,14 +140,12 @@ async function renderBookings(bookings){
 		}
 	});
 
-	// create pdf to print Schedule
-	makeSchedulePDF(sorted);
 }
 
 function makeSchedulePDF(bookings) {
 	var pdf = new jsPDF();
 	pdf.setFontSize(25);
-	pdf.text(today.format("dddd Do MMMM"), 20, 20);
+	pdf.text(moment().format("dddd Do MMMM"), 20, 20);
 	pdf.setFontSize(20);
 	var row = 0;
 	bookings.forEach(function (ticket, index) {
@@ -124,6 +162,25 @@ function makeSchedulePDF(bookings) {
 	$("button[name=print]").on('click', function () {
 		pdf.save('schedule.pdf');
 		//pdf.autoPrint();
+	});
+}
+
+function renderLinks(shop) {
+	// copy link button
+	new ClipboardJS('#copy-link');
+	$("#copy-link").attr('data-clipboard-text', 'https://conga.store/book?shop=' + shop.data().code);
+	// qr code and pdf poster
+	var qrcode = new QRCode(document.getElementById("qrcode"), 'https://conga.store/book?shop=' + shop.data().code);
+	$("#qrcode > img ").on('load', function() {
+		var pdf = new jsPDF();
+		pdf.setFontSize(40);
+		pdf.text("Book your visit", 30, 30);
+		pdf.text("with Conga", 30, 50);
+		pdf.addImage(this.src, 30, 100, 100, 100);
+		pdf.text('www.conga.store/' + shop.data().code, 30, 250);
+		$("#get-pdf").on('click', function() {
+			pdf.save('Shop poster ('+ shop.data().code[0] + "-" + shop.data().code[1] + "-" + shop.data().code[2] + "-" + shop.data().code[3] + ')');
+		});
 	});
 }
 
--- client/js/index.js
+++ client/js/index.js
@@ -21,6 +21,7 @@ $(document).ready(async function () {
 		  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 		  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 		  var modal = $(this);
+			modal.removeClass('text-dark').removeClass('text-white').addClass(lightOrDark(ticket.css('background-color')) ? 'text-dark':'text-white');
 		  modal.find('#shop-name-focus').text(ticket.parent().find('#shop-name').text());
 		  modal.find('#ticket-date-focus').text(ticket.find('h6')[0].textContent);
 			modal.find('#ticket-time-focus').text(ticket.find('h6')[1].textContent);
@@ -38,25 +39,33 @@ async function renderWallet() {
 	var sorted = Object.entries(cookies);
 	sorted.sort((a, b) => moment(a[1].date + ' ' + a[1].time, 'DD/MM/YYYY HH:mm') - moment(b[1].date + ' ' + b[1].time, 'DD/MM/YYYY HH:mm'));
 
-
 	var wallet = $("#wallet");
 	wallet.empty();
-	$.each(Object.fromEntries(sorted), async function (id, data) {
-		// only display bookings for tomorrow - others will expire in the background
-		if (data.date == tomorrow.format("DD/MM/YYYY") || data.date == today.format("DD/MM/YYYY")){ 
-			console.log(id, data);
-			var shop = await db.collection('shops').doc(data.shop).get();
-			wallet.append('<div class="col-sm-12 col-md-6 mb-5"><h4 class="pb-3" id="shop-name">' + shop.data().name + '</h4><div class="card border-light ' + (lightOrDark(data.color) ? 'text-dark':'text-white') + ' text-center" style="background-color:' + data.color + '" data-toggle="modal" data-target="#ticket-focus"><div class="card-body"><h6 class="card-subtitle mb-2 ' + (lightOrDark(data.color) ? 'text-dark':'text-white') + '" style="float:left">' + moment(data.date, "DD/MM/YYYY").format("dddd Do MMMM") + '</h6><h6 class="card-subtitle mb-2 ' + (lightOrDark(data.color) ? 'text-dark':'text-white') + '" style="float:right">' + data.time + '</h6><div style="clear: both;"></div><h1 class="card-title display-4 font-weight-bold mt-3">#' + data.time.replace(':', '') + '</h1></div></div><button type="button" name="cancel" class="btn btn-secondary w-100 my-2" booking="' + id + '">Cancel booking</button></div>');
+	$.each(Object.fromEntries(sorted), async function (id, booking) {
+		// only display bookings for today tomorrow - others will expire in the background
+		if (booking.date == tomorrow.format("DD/MM/YYYY") || (booking.date == today.format("DD/MM/YYYY") && booking.time >= moment().format('HH:mm'))){ 
+			console.log(id, booking);
+			var shop = await db.collection('shops').doc(booking.shop).get();
+			wallet.append('<div class="col-sm-12 col-md-6 mb-5"><h4 class="pb-3" id="shop-name">' + shop.data().name + '</h4><div class="card border-light ' + (lightOrDark(booking.color) ? 'text-dark':'text-white') + ' text-center" style="background-color:' + booking.color + '" data-toggle="modal" data-target="#ticket-focus"><div class="card-body"><h6 class="card-subtitle mb-2 ' + (lightOrDark(booking.color) ? 'text-dark':'text-white') + '" style="float:left">' + moment(booking.date, "DD/MM/YYYY").format("dddd Do MMMM") + '</h6><h6 class="card-subtitle mb-2 ' + (lightOrDark(booking.color) ? 'text-dark':'text-white') + '" style="float:right">' + booking.time + '</h6><div style="clear: both;"></div><h1 class="card-title display-4 font-weight-bold mt-3">#' + booking.time.replace(':', '') + '</h1></div></div><button type="button" name="cancel" class="btn btn-secondary w-100 my-2" booking="' + id + '">Cancel booking</button></div>');
+
+			console.log(booking.time);
+			var diff = moment.duration(moment(booking.time, "HH:mm") - moment()).asMinutes();
+			console.log(diff);
+			if ((booking.date == today.format("DD/MM/YYYY")) && (diff > 0) && (diff < 2)) {
+				$(':button[booking="' + id + '"]').prev().click();
+			}
 		}
+
+
 	});
 
 	var favourites = $('#favourites');
 	favourites.empty();
-	$.each(cookies, function(id, data) {
-		if ((data.address != undefined) && (sorted.find(item => item[1].shop == id) == undefined)) {
-			console.log(id, data);
-			favourites.append('<div class="col-sm-12 col-md-6 mb-5"><h4 class="pb-3" id="shop-name">' + data.name + '</h4><button type="button" name="book" code="' + data.code + '" class="btn btn-secondary w-100 mb-2">Make booking</button></h4><button type="button" name="forget" id="' + id + '" class="btn btn-secondary w-100 mb-2">Remove favourite</button></div>');
-			$(':button[code=' + data.code + ']').attr('onclick', 'window.location="/book?shop=' + data.code + '"');
+	$.each(cookies, function(id, shop) {
+		if ((shop.address != undefined) && (sorted.find(item => item[1].shop == id) == undefined)) {
+			console.log(id, shop);
+			favourites.append('<div class="col-sm-12 col-md-6 mb-5"><h4 class="pb-3" id="shop-name">' + shop.name + '</h4><button type="button" name="book" code="' + shop.code + '" class="btn btn-secondary w-100 mb-2">Make booking</button></h4><button type="button" name="forget" id="' + id + '" class="btn btn-secondary w-100 mb-2">Remove favourite</button></div>');
+			$(':button[code=' + shop.code + ']').attr('onclick', 'window.location="/book?shop=' + shop.code + '"');
 			$(':button[id=' + id + ']').click(function() {
 				Cookies.remove(id);
 				console.log('deleted cookie with id:', id);
@@ -64,6 +73,8 @@ async function renderWallet() {
 			})
 		}
 	});
+
+
 
 
 
